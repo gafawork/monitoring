54989389d50743ba752a1156bbdc3c299b228eaf
gafawork <tirso.andrade@gmail.com>
2025-08-24 15:25:32 -0300

    Enhance SQL debugging aspect with proper annotations and configuration

--- monitoring-debug-sql-starter/src/main/java/gafawork/debugsql/config/DebugSqlConfig.java
+++ monitoring-debug-sql-starter/src/main/java/gafawork/debugsql/config/DebugSqlConfig.java
@@ -1,6 +1,7 @@
 package gafawork.debugsql.config;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("gafawork.debugsql")
--- monitoring-debug-sql/src/main/java/gafawork/debugsql/DebugSqlAspect.java
+++ monitoring-debug-sql/src/main/java/gafawork/debugsql/DebugSqlAspect.java
@@ -1,7 +1,8 @@
 package gafawork.debugsql;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
